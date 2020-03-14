26bc73c04f0c22236cbd0396e0b11ce56c09bd8a
Saad <49029610+saad-khannn@users.noreply.github.com>
2020-03-14 18:46:33 -0400

    testing, scene switches, and menu buttons
    testing to make sure that scenes switched properly. buttons for menu are highlighted once selected. keyboard navigation for menu so user can switch between menu options with arrow keys and select an option with enter key. redundant scene switch scripts were deleted and one script that handles all scene switches from menu was added.

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public String sceneToChange;
+    public void NextScene()
+    {
+        SceneManager.LoadScene(sceneToChange);
+    }
+}
