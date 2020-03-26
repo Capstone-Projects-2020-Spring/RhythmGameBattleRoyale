30663fd83a3e154ccb84213876651bcea2231d07
Saad <49029610+saad-khannn@users.noreply.github.com>
2020-03-26 19:48:18 -0400

    lobby region
    there was a problem where different players running the build on different machines would be given their own lobbies instead of being put into the same lobby. fixed this problem by adding "us" as the fixed region under photon server settings. also added Debug.LogError lines to debug in development build, so these lines can be removed later and replaced with normal Debug.Log lines.

--- Assets/PhotonLobby.cs
+++ Assets/PhotonLobby.cs
@@ -19,17 +19,20 @@ public class PhotonLobby : MonoBehaviourPunCallbacks
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.ConnectToRegion("us");
     }
 
     public override void OnConnectedToMaster()
     {
-        Debug.Log("Player has connected to the Photon master server");
+        //Debug.Log("Player has connected to the Photon master server");
+        Debug.LogError("Player has connected to the Photon master server");
         connectButton.SetActive(true);
     }
 
     public void OnConnectButtonClicked()
     {
-        Debug.Log("Attempting to connect to lobby");
+        //Debug.Log("Attempting to connect to lobby");
+        Debug.LogError("Attempting to connect to lobby");
         connectButton.SetActive(false);
         cancelButton.SetActive(true);
         PhotonNetwork.JoinRandomRoom();
@@ -37,32 +40,39 @@ public class PhotonLobby : MonoBehaviourPunCallbacks
 
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
-        Debug.Log("Joining a random lobby failed. No open lobbies available");
+        //Debug.Log("Joining a random lobby failed. No open lobbies available");
+        Debug.LogError("Joining a random lobby failed. No open lobbies available");
         CreateRoom();
     }
 
     void CreateRoom()
     {
-        Debug.Log("Trying to create a lobby");
+        //Debug.Log("Trying to create a lobby");
+        Debug.LogError("Trying to create a lobby");
         int randomRoomName = Random.Range(0, 1000);
         RoomOptions roomOptions = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 8 };
         PhotonNetwork.CreateRoom("Lobby " + randomRoomName, roomOptions);
+        //Debug.Log("Lobby created");
+        Debug.LogError("Lobby created");
     }
 
     public override void OnJoinedRoom()
     {
-        Debug.Log("Player is now in a lobby");
+        //Debug.Log("Player is now in a lobby");
+        Debug.LogError("Player is now in a lobby");
     }
 
     public override void OnCreateRoomFailed(short returnCode, string message)
     {
-        Debug.Log("Lobby could not be created because lobby with same name already exists.");
+        //Debug.Log("Lobby could not be created because lobby with same name already exists.");
+        Debug.LogError("Lobby could not be created because lobby with same name already exists.");
         CreateRoom();
     }
 
     public void OnCancelButtonClicked()
     {
-        Debug.Log("Stopping search for lobbies");
+        //Debug.Log("Stopping search for lobbies");
+        Debug.LogError("Stopping search for lobbies");
         cancelButton.SetActive(false);
         connectButton.SetActive(true);
         PhotonNetwork.LeaveRoom();
@@ -71,6 +81,7 @@ public class PhotonLobby : MonoBehaviourPunCallbacks
     public void DisconnectPlayer()
     {
         PhotonNetwork.Disconnect();
-        Debug.Log("Disconnected");
+        //Debug.Log("Disconnected");
+        Debug.LogError("Disconnected");
     }
 }
