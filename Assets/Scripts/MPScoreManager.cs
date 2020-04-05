abec1d350d5fed5558c92e159e37f2459a5955bf
Vinayak Desai <vinayalaw@gmail.com>
2020-04-05 17:31:56 -0400

    Started the scoretracker

---
+++ Assets/Scripts/MPScoreManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MPScoreManager : MonoBehaviour
+{
+    public GameObject scorePanel;
+
+    void Start()
+    {
+        
+    }
+    //use PUN player object properties to update other player scores and client score
+    void Update()
+    {
+        
+    }
+}
